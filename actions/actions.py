8e4aace486fc22c6d390423e5f036cdb7f57639e
Lívia Coutinho <liviacoutinhog22@gmail.com>
2025-02-25 17:20:20 -0300

    Adicionado requirements, modificado actions

--- actions/actions.py
+++ actions/actions.py
@@ -1,6 +1,7 @@
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from typing import Any, Text, Dict, List
+import requests
 
 class ActionHelloWorld(Action):
 
@@ -12,5 +13,31 @@ class ActionHelloWorld(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         dispatcher.utter_message(text="Hello World!")
+ 
+        return []
+
+class ActionProcuraLivros(Action):
+
+    apiGoogleBooks = "https://www.googleapis.com/books/v1/volumes?q="
+
+    def name(self) -> Text:
+        return "action_procura_livros"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        livro = tracker.get_slot('livro')
+        livro_pesquisa = livro.replace(" ", "+")
+        print(livro_pesquisa)
+
+        Pesquisa_Livro = requests.get(f"{self.apiGoogleBooks}{livro_pesquisa}")
+        livros_json = Pesquisa_Livro.json()
+
+        # Extrair todos os títulos dos livros
+        titulos = [item['volumeInfo']['title'] for item in livros_json.get('items', [])]
+
+        # Enviar os títulos de volta ao usuário
+        dispatcher.utter_message(text=f"Os títulos dos livros encontrados são: {', '.join(titulos)}")
 
         return []
